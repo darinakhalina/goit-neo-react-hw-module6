15830b0a1d748cf98470307c7a24a3b7afcf9bbf
Darina Khalina <darina.khalina@namecheap.com>
2024-10-15 15:33:12 +0300

    chore: add redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import 'modern-normalize';
 import './index.css';
 import App from './components/App/App';
@@ -9,7 +10,9 @@ import App from './components/App/App';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
