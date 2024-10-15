9e82db78e342ec221277149b4c03a13cfc608598
Darina Khalina <darina.khalina@namecheap.com>
2024-10-15 14:55:25 +0300

    chore: add filteredContacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,24 @@
 import { useSelector } from 'react-redux';
 import { selectContacts } from '../../redux/contactsSlice';
+import { selectNameFilter } from '../../redux/filtersSlice';
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
 
 function ContactList() {
   const contacts = useSelector(selectContacts);
+  const nameFilter = useSelector(selectNameFilter);
 
-  if (!contacts || contacts.length === 0) {
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(nameFilter.toLowerCase())
+  );
+
+  if (filteredContacts.length === 0) {
     return <p>No contacts found.</p>;
   }
+
   return (
     <ul className={css['contact-list']}>
-      {contacts.map(contact => (
+      {filteredContacts.map(contact => (
         <li className={css['contact-list-item']} key={contact.id}>
           <Contact contact={contact} />
         </li>
